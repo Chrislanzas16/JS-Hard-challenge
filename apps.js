36b7edaecaf48f9b42efcc6a0a10c16885c5826a
Chris <chrislanzas89@outlook.com>
2025-07-11 14:58:39 -0400

    Sort Array low to high

--- apps.js
+++ apps.js
@@ -1,16 +1,7 @@
-function showRating (rating) {
-    let ratings = "";
-  for (i = 0; i < Math.floor(rating); ++i){
-   ratings += "*"
-   if (i !== Math.floor (rating) - 1) {
-    ratings += " "
-   }
-  }
-  if ( !Number.isInteger(rating)) {
-    ratings += " .";
-
-  }
- return ratings;
+function sortLowToHigh(arr) {
+return arr.sort((a, b) => a - b)
 }
 
-console.log(showRating(3.5));
+
+
+console.log(sortLowToHigh([1, 5, 0, 10, 4]))
